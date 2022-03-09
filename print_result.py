d3acdac5c1424e36fc70f037e8d166c9ac4bea4f
Chenxi Ou <kryst3154@outlook.com>
2022-03-08 21:24:30 -0800

    Restructured print_result so that it handles when only one result folder is created.

--- print_result.py
+++ print_result.py
@@ -1,25 +1,35 @@
 from puncta_detect_util import print_result, manual_print_result
 import os
+from os.path import join
 import pandas as pd
 import numpy as np
 
 channels_of_interest = [0, 1]
 result_files = []
+if not os.path.exists("results"):
+    os.mkdir("results")
 for file in os.listdir("results"):
-    if file.endswith(".csv"):
-        result_files.append(os.path.join("results", file))
+    if not file.endswith(".csv"):
+        result_files.append(join("results", file))
 
-for result_csv_file in result_files:
-    print("Starting on output file", result_csv_file)
-    print_result(pd.read_pickle(result_csv_file[:-4]), channels_of_interest)
+for result_file in result_files:
+    print("Starting on output file", result_file)
+    result_df = pd.read_pickle(result_file)
+    if not os.path.exists(f"{result_file}.csv"):
+        result_df.to_csv(f"{result_file}.csv", index=False)
+    print_result(result_df, channels_of_interest)
 
+if not os.path.exists("manual_results"):
+    os.mkdir("manual_results")
 manual_channels_of_interest = [0, 1, 2]
 result_files = []
 for file in os.listdir("manual_results"):
-    if file.endswith(".csv"):
-        result_files.append(os.path.join("manual_results", file))
+    if not file.endswith(".csv"):
+        result_files.append(join("manual_results", file))
 
-for result_csv_file in result_files:
-    print("Starting on manual output file", result_csv_file)
-    manual_label_df = pd.read_pickle(result_csv_file[:-4])
+for result_file in result_files:
+    print("Starting on manual output file", result_file)
+    manual_label_df = pd.read_pickle(result_file)
+    if not os.path.exists(f"{result_file}.csv"):
+        manual_label_df.to_csv(f"{result_file}.csv", index=False)
     manual_print_result(manual_label_df, manual_channels_of_interest)
